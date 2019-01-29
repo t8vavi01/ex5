6e01dde8146182cf45302128cf1be4858521560f
Ville Valta <t8vavi01@students.oamk.fi>
2019-01-29 21:50:57 +0200

    Tehtävä valmis

--- myFunctions.js
+++ myFunctions.js
@@ -2,7 +2,7 @@ function loadDoc(page) {
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("menu").innerHTML = this.responseText;
+    document.getElementById("content").innerHTML = this.responseText;
     }
 };
 xhttp.open("GET", page, true);
